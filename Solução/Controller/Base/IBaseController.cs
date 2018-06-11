d6f6ff1bde58e861cad07e16d9df7ae7e3576b57
Guilherme Mendes <guuiilherme@hotmail.com>
2018-06-11 01:43:50 -0300

    Base e Controller criados.

---
+++ Solução/Controller/Base/IBaseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller.Base
+{
+    interface IBaseController<Usuario> : class
+    {
+        void Adicionar(T entity);
+
+        List<T> ListarTodos();
+
+        List<T> ListarPorNome(String nome);
+
+        T BuscarPorID(int id);
+
+        void Editar(T entity);
+
+        void Excluir(int id);
+
+
+}
+}
--- Solução/Controller/UsuarioController.cs
+++ Solução/Controller/UsuarioController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Controller.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace Controller
 {
-    public class Usuario
+    public class UsuarioController<Usuario> : IBaseController<Usuario>
     {
-        //
+        
     }
 }
